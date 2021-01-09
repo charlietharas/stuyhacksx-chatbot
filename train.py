026c277cff3e0990c00b174bf6f069f575aaaa52
Charlie Tharas <charlie@charliemax.dev>
2021-01-09 18:36:14 -0500

    Added small data analysis feature

--- train.py
+++ train.py
@@ -121,6 +121,7 @@ if feature_analysis:
                 total_word_count += 1
     
     wordset = set(words)
+    print("Total words from " + sel_ser + ": " + str(total_word_count))
     print("Total messages from " + sel_user + ": " + str(usr_message_count))
     
     for i in wordset:
